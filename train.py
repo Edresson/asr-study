524c8c98a7a20e04e55414a21073884c1625b5ef
dresson <edresson1@gmail.com>
2018-05-12 13:38:09 -0300

    update

--- train.py
+++ train.py
@@ -223,8 +223,7 @@ if __name__ == '__main__':
     #print('train_flow ',train_flow[0].next())
     # Fit the model
     #print(next(valid_flow))
-    with tf.device('/gpu:0'):
-        model.fit_generator(train_flow, steps_per_epoch=steps_per_epoch,
+    model.fit_generator(train_flow, steps_per_epoch=steps_per_epoch,
                             epochs=args.num_epochs, validation_data=valid_flow,
                             validation_steps=validation_steps, max_queue_size=10,
                             workers=1, callbacks=callback_list, verbose=1,
