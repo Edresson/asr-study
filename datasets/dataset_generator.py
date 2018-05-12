1af44b358a6046d723ad49fbe83a9f857b1af177
dresson <edresson1@gmail.com>
2018-05-12 14:05:23 -0300

    update

--- datasets/dataset_generator.py
+++ datasets/dataset_generator.py
@@ -181,9 +181,8 @@ class DatasetIterator(Iterator):
         return len(self.inputs)
 
     def _get_batches_of_transformed_samples(self, index_array):
-        print(len(index_array))
         
-        index_array, current_index, current_batch_size = index_array
+        current_batch_size = len(index_array)
         
         index_array.sort()
 
@@ -219,11 +218,25 @@ class DatasetIterator(Iterator):
         #Keras 2 support.
         with self.lock:
             
-            index_array= next(self.index_generator)
+            index_array, current_index, current_batch_size= next(self.index_generator)
 
         
+        index_array.sort()
 
-        return self._get_batches_of_transformed_samples(index_array)
+        index_array_list = index_array.tolist()
+
+        batch_inputs, batch_inputs_len = self._make_in(
+            self.inputs[index_array_list], current_batch_size)
+
+        if self.labels is not None:
+            batch_labels = self._make_out(self.labels[index_array_list],
+                                          current_batch_size)
+        else:
+            batch_labels = None
+
+        return self._make_in_out(batch_inputs, batch_labels, batch_inputs_len)
+
+
 
     def _make_in_out(self, batch_inputs, batch_labels, batch_inputs_len=None):
         # if label is not provided output is not necessary
